0ff0086d348a2d6540dd544bd69214af8abd308d
tyriem <tyrie.moss@gmail.com>
2021-07-06 12:05:05 -0400

    UPDATED HELLO WORLD TO ADD CARD FUNCTIONALITY

--- ION 1 - HelloWorld/src/pages/Home.tsx
+++ ION 1 - HelloWorld/src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -12,15 +11,35 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
+
+          {/* TOOL BAR COMPONENT */}
           <IonToolbar>
             <IonTitle size="large">TMRM: HelloWorld</IonTitle>
           </IonToolbar>
         </IonHeader>
+
+        {/* RAW DIVs */}
         <div><h1>HELLO WORLD!</h1></div>
         <div><h2>VERSION 2.0 NOW WITH FAST RELOAD!</h2></div>
-        
 
-        <ExploreContainer />
+        <div><ul>
+          <li>Start Ionic</li>
+          <li>Serve Ionic</li>
+          <li>Build Ionic</li>
+          </ul></div>
+        
+          {/* CARD COMPONENT */}
+          <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>THIS IS A SUBTITLE FOR THE HELLO WORLD CARD</IonCardSubtitle>
+            <IonCardTitle>HELLO WORLD CARD</IonCardTitle>
+          </IonCardHeader>
+
+          <IonCardContent>
+            This is the content for the Hello World Card. LOREM IPSUM. LOREM IPSUM!
+      </IonCardContent>
+        </IonCard>
+      
       </IonContent>
     </IonPage>
   );
